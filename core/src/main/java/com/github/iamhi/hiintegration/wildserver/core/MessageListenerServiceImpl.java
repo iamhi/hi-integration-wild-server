8030a75bc2dc6c4b6c27d0d56a24a7c3ecc04512
iamhi <inpactt@gmail.com>
2022-04-10 23:32:19 +0200

    Message channels

--- core/src/main/java/com/github/iamhi/hiintegration/wildserver/core/MessageListenerServiceImpl.java
+++ core/src/main/java/com/github/iamhi/hiintegration/wildserver/core/MessageListenerServiceImpl.java
@@ -5,8 +5,13 @@ import com.github.iamhi.hiintegration.wildserver.data.ChannelRepository;
 import com.github.iamhi.hiintegration.wildserver.data.RedisPubSubWrapper;
 import com.github.iamhi.hiintegration.wildserver.data.TokenRepository;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Service
 record MessageListenerServiceImpl(
@@ -15,12 +20,16 @@ record MessageListenerServiceImpl(
     TokenRepository tokenRepository,
     Gson gson
 ) implements MessageListenerService {
+
+    private static final String DEFAULT_CHANNEL = "all";
+    private static final String CHANNEL_PROPERTY = "channel";
+
     @Override
     public Mono<Boolean> handleUserInput(UserInputDTO userInput) {
-        System.out.println(userInput.message());
+        String channel = getChannelForUserInput(userInput);
+        String message = getMessageForUserInput(userInput);
 
-        return redisPubSubWrapper.publish("all",
-            userInput.token() + ":" + userInput.message()).map(result -> true);
+        return redisPubSubWrapper.publish(channel, message).map(result -> true);
     }
 
     @Override
@@ -52,5 +61,32 @@ record MessageListenerServiceImpl(
             .forEach(channel -> channelRepository.removeToken(channel, token));
 
         return Mono.empty();
+    }
+
+    private String getChannelForUserInput(UserInputDTO userInput) {
+        try {
+            UserMessage userMessage = gson.fromJson(userInput.message(), UserMessage.class);
+
+            return StringUtils.defaultString(userMessage.channel, DEFAULT_CHANNEL);
+        } catch (JsonSyntaxException exception) {
+            return DEFAULT_CHANNEL;
+        }
+    }
+
+    private String getMessageForUserInput(UserInputDTO userInputDTO) {
+        Map<String, String> messageProperties = gson.<Map<String, String>>fromJson(userInputDTO.message(), Map.class);
+
+        messageProperties.put("token", userInputDTO.token());
+
+
+        return gson().toJson(messageProperties);
+    }
+
+    @Data
+    private static class UserMessage {
+
+        private String channel;
+
+        private String data;
     }
 }
